d9c75eed6fb34f07d2c6a9e4bb83f261b4bb442d
Richard Han <richard@mentad.com>
2015-12-15 15:55:55 -0800

    Add temp

--- index.js
+++ index.js
@@ -2,12 +2,31 @@
 
 var request = require('superagent');
 
+function sameOrigin(url) {
+  // url could be relative or scheme relative or absolute
+  var host = document.location.host; // host + port
+  var protocol = document.location.protocol;
+  var sr_origin = '//' + host;
+  var origin = protocol + sr_origin;
+  // Allow absolute or scheme relative URLs to same origin
+  return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+      (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        // or any other URL that isn't scheme relative or absolute i.e relative.
+      !(/^(\/\/|http:|https:).*/.test(url));
+}
+
+function safeMethod(method) {
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
 // patch superagent to attach CSRF-token to all requests
 try {
   var csrf = document.cookie.match(/csrftoken=(.*?)(?:$|;)/)[1];
   var end = request.Request.prototype.end;
   request.Request.prototype.end = function(fn) {
-    this.set('X-CSRFToken', csrf);
+    if(!safeMethod(this.method) && sameOrigin(this.url)) {
+      this.set('X-CSRFToken', csrf);
+    }
     return end.call(this, fn);
   };
 }
